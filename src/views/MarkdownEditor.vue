1f3e2bbaca6a00ee648aa0bba6203823c240b346
MarwanAhmedElAbhar <marwan.a.elabhar@gmail.com>
2022-12-03 10:59:34 +0200

    add markdown editor view and route

---
+++ src/views/MarkdownEditor.vue
@@ -0,0 +1,20 @@
+<script>
+import MarkdownEditor from "@/components/MarkdownEditor.vue";
+import AppLayout from "@/components/AppLayout.vue";
+export default {
+  components: {
+    AppLayout,
+    MarkdownEditor,
+  },
+
+  data() {
+    return {};
+  },
+};
+</script>
+<template>
+  <AppLayout>
+    <MarkdownEditor />
+  </AppLayout>
+</template>
+<style scoped lang='sass'></style>
