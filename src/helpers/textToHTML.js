cf470be5e220dd3f50cba5281a8492514038120e
MarwanAhmedElAbhar <marwan.a.elabhar@gmail.com>
2022-12-17 01:08:29 +0200

    add line through markdown

--- src/helpers/textToHTML.js
+++ src/helpers/textToHTML.js
@@ -10,6 +10,12 @@ export default function textToHTML(text) {
         .replace(/\*(.*)\*/gim, '<i>$1</i>') // italic text
         .replace(/\[(.*?)\]\((.*?)\)/g, '<img src="$2">') // image
         .replace(/\n/g, '<br>') // new line
+        .replace(/~~(.+?)~~/g, '<p style="text-decoration: line-through;">$1</p>')
+        .replace(/`(.+)`/g, '<code>$1</code>')
+        // .replace(/^>(.+)/gm, '<blockquote>$1</blockquote>')
+        // .replace(/^\s*\n\d\./gm, '<ol>\n1.')
+//   .replace(/^(\d\..+)\s*\n([^\d\.])/gm, '$1\n</ol>\n\n$2')
+//   .replace(/[`]{1}([^`]+)[`]{1}/g, '<code>$1</code>')
 
     return toHTML;
 
